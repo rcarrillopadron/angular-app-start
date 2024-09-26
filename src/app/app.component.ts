467e393dc21bf08108c7a229c18d3295b3f7d8ec
Roberto Carrillo <email@robertocarrillo.com>
2024-09-25 22:03:44 -0500

    Added Home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   standalone: true,
@@ -9,9 +10,13 @@ import { Component } from "@angular/core";
         <img class="brand-logo" src="/assets/logo.svg"
           alt="logo" aria-hidden="true" />
       </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
     <main>
   `,
   styleUrls: ["./app.component.css"],
+  imports: [HomeComponent],
 })
 export class AppComponent {
   title = "homes";
